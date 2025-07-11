a7a7ba620441beb14c6aa586544ae843ab6a22ff
George-Tsiamasiotis <gt.develop@protonmail.com>
2025-07-11 17:27:34 +0300

    check: added InterpolationType wrapper

---
+++ gsl-splines/src/lib.rs
@@ -0,0 +1,6 @@
+mod interp_types;
+
+// Types refering to rgsl's Interp and Spline types, to avoid confusion.
+pub(crate) type RgslInterpType = rgsl::InterpType;
+
+pub use interp_types::InterpolationType;
