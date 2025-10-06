574d80a798d0df1351ca15b2d23776b542d8705a
George-Tsiamasiotis <gt.develop@protonmail.com>
2025-10-06 12:15:18 +0200

    check: wrote general formula for RK45, added Classic method coefs

--- src/rk45.rs
+++ src/rk45.rs
@@ -4,6 +4,9 @@ use crate::Qfactor;
 use crate::Result;
 use crate::State;
 
+// Hardcoded. Change this to switch between methods.
+use self::classic_tableau::*;
+
 #[derive(Debug)]
 pub(crate) struct Rk45State {
     pub(crate) k1: [f64; 4],
@@ -61,11 +64,18 @@ impl Rk45State {
         bfield: &Bfield,
         current: &Current,
     ) -> Result<()> {
-        self.state2.t = self.state1.t + h / 2.0;
-        self.state2.theta = self.state1.theta + h / 2.0 * self.k1[0];
-        self.state2.psip = self.state1.psip + h / 2.0 * self.k1[1];
-        self.state2.rho = self.state1.rho + h / 2.0 * self.k1[2];
-        self.state2.zeta = self.state1.zeta + h / 2.0 * self.k1[3];
+        let coef = [
+            A21 * self.k1[0],
+            A21 * self.k1[1],
+            A21 * self.k1[2],
+            A21 * self.k1[3],
+        ];
+
+        self.state2.t = self.state1.t + C2 * h;
+        self.state2.theta = self.state1.theta + coef[0] * h;
+        self.state2.psip = self.state1.psip + coef[1] * h;
+        self.state2.rho = self.state1.rho + coef[2] * h;
+        self.state2.zeta = self.state1.zeta + coef[3] * h;
         self.state2.evaluate(qfactor, current, bfield)?;
         self.k2 = [
             self.state2.theta_dot,
@@ -83,11 +93,17 @@ impl Rk45State {
         bfield: &Bfield,
         current: &Current,
     ) -> Result<()> {
-        self.state3.t = self.state1.t + h / 2.0;
-        self.state3.theta = self.state1.theta + h / 2.0 * self.k2[0];
-        self.state3.psip = self.state1.psip + h / 2.0 * self.k2[1];
-        self.state3.rho = self.state1.rho + h / 2.0 * self.k2[2];
-        self.state3.zeta = self.state1.zeta + h / 2.0 * self.k2[3];
+        let coef = [
+            A31 * self.k1[0] + A32 * self.k2[0],
+            A31 * self.k1[1] + A32 * self.k2[1],
+            A31 * self.k1[2] + A32 * self.k2[2],
+            A31 * self.k1[3] + A32 * self.k2[3],
+        ];
+        self.state3.t = self.state1.t + C3 * h;
+        self.state3.theta = self.state1.theta + coef[0] * h;
+        self.state3.psip = self.state1.psip + coef[1] * h;
+        self.state3.rho = self.state1.rho + coef[2] * h;
+        self.state3.zeta = self.state1.zeta + coef[3] * h;
         self.state3.evaluate(qfactor, current, bfield)?;
         self.k3 = [
             self.state3.theta_dot,
@@ -105,11 +121,17 @@ impl Rk45State {
         bfield: &Bfield,
         current: &Current,
     ) -> Result<()> {
-        self.state4.t = self.state1.t + h;
-        self.state4.theta = self.state1.theta + h * self.k3[0];
-        self.state4.psip = self.state1.psip + h * self.k3[1];
-        self.state4.rho = self.state1.rho + h * self.k3[2];
-        self.state4.zeta = self.state1.zeta + h * self.k3[3];
+        let coef = [
+            A41 * self.k1[0] + A42 * self.k2[0] + A43 * self.k3[0],
+            A41 * self.k1[1] + A42 * self.k2[1] + A43 * self.k3[1],
+            A41 * self.k1[2] + A42 * self.k2[2] + A43 * self.k3[2],
+            A41 * self.k1[3] + A42 * self.k2[3] + A43 * self.k3[3],
+        ];
+        self.state4.t = self.state1.t + C4 * h;
+        self.state4.theta = self.state1.theta + coef[0] * h;
+        self.state4.psip = self.state1.psip + coef[1] * h;
+        self.state4.rho = self.state1.rho + coef[2] * h;
+        self.state4.zeta = self.state1.zeta + coef[3] * h;
         self.state4.evaluate(qfactor, current, bfield)?;
         self.k4 = [
             self.state4.theta_dot,
@@ -122,7 +144,7 @@ impl Rk45State {
 
     pub(crate) fn calculate_weights(&mut self) {
         for i in 0..self.weights.len() {
-            self.weights[i] = (self.k1[i] + 2.0 * self.k2[i] + 2.0 * self.k3[i] + self.k4[i]) / 6.0;
+            self.weights[i] = B1 * self.k1[i] + B2 * self.k2[i] + B3 * self.k3[i] + B4 * self.k4[i];
         }
     }
 
@@ -155,4 +177,26 @@ impl Default for Rk45State {
             next: State::new_uninit(),
         }
     }
+}
+
+mod classic_tableau {
+
+    pub(super) const B1: f64 = 1.0 / 6.0;
+    pub(super) const B2: f64 = 1.0 / 3.0;
+    pub(super) const B3: f64 = 1.0 / 3.0;
+    pub(super) const B4: f64 = 1.0 / 6.0;
+
+    // pub(super) const C1: f64 = 0.0; Always equals to 0.
+    pub(super) const C2: f64 = 1.0 / 2.0;
+    pub(super) const C3: f64 = 1.0 / 2.0;
+    pub(super) const C4: f64 = 1.0;
+
+    pub(super) const A21: f64 = 1.0 / 2.0;
+
+    pub(super) const A31: f64 = 0.0;
+    pub(super) const A32: f64 = 1.0 / 2.0;
+
+    pub(super) const A41: f64 = 0.0;
+    pub(super) const A42: f64 = 0.0;
+    pub(super) const A43: f64 = 1.0;
 }
