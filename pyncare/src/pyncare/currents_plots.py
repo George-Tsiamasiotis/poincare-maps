da7b8b5d9092776e98e4d8ae505753794201c2ae
George-Tsiamasiotis <gt.develop@protonmail.com>
2025-11-09 13:42:52 +0100

    plot: currents_plots now plots g and i by interpolation

--- pyncare/src/pyncare/currents_plots.py
+++ pyncare/src/pyncare/currents_plots.py
@@ -11,13 +11,15 @@ def g_plot(ax: Axes, current: Currents):
     g_data = current.g_data
     # Smooth derivative curve
     psips = np.linspace(current.psip_data[0], current.psip_data[-1], 1000)
+    g = np.zeros((len(psips)))
     dg_dpsip = np.zeros((len(psips)))
-    for i in range(len(dg_dpsip)):
-        dg_dpsip[i] = current.dg_dpsip(psips[i])
+    for n in range(len(dg_dpsip)):
+        g[n] = current.g(psips[n])
+        dg_dpsip[n] = current.dg_dpsip(psips[n])
 
     dax = ax.twinx()
     ax.scatter(psip_data, g_data, c="k", s=2, zorder=2, alpha=0.8, label="data points")
-    ax.plot(psip_data, g_data, c="b", label=r"$g(\psi_p)$")
+    ax.plot(psips, g, c="b", label=r"$g(\psi_p)$")
     dax.plot(psips, dg_dpsip, c="r", label=r"$\partial g(\psi_p)\partial \psi_p$")
 
     ax.set_xlabel(r"$\psi_p$")
@@ -31,15 +33,17 @@ def g_plot(ax: Axes, current: Currents):
 def i_plot(ax: Axes, current: Currents):
     psip_data = current.psip_data
     i_data = current.i_data
+    # Smooth derivative curve
     psips = np.linspace(current.psip_data[0], current.psip_data[-1], 1000)
     di_dpsip = np.zeros((len(psips)))
-    # Smooth derivative curve
-    for i in range(len(di_dpsip)):
-        di_dpsip[i] = current.di_dpsip(psips[i])
+    i = np.zeros((len(psips)))
+    for n in range(len(di_dpsip)):
+        i[n] = current.i(psips[n])
+        di_dpsip[n] = current.di_dpsip(psips[n])
 
     dax = ax.twinx()
     ax.scatter(psip_data, i_data, c="k", s=2, zorder=2, alpha=0.8, label="data points")
-    ax.plot(psip_data, i_data, c="b", label=r"$I(\psi_p)$")
+    ax.plot(psips, i, c="b", label=r"$I(\psi_p)$")
     dax.plot(psips, di_dpsip, c="r", label=r"$\partial I(\psi_p)\partial \psi_p$")
 
     ax.set_ylabel(r"$I(\psi_p)$")
